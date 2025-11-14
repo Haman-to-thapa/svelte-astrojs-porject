4d3346fbf10773ef7b667d51519881c79114832a
Haman-to-thapa <hamanthapa0000@gmail.com>
2025-11-14 13:22:16 +0530

    tailwind class fixed

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-  },
-  preview: {
-    port: 3000,
-    host: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
+    }
   }
 })
